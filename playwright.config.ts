16d7ef92b39253aa744f68bb93ac4778b7248ef8
Mohammed Lukmanudhin <lukmancolan@gmail.com>
2026-07-10 15:20:58 +0530

    fix: update trace configuration for CI environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
+    trace: process.env.CI ? 'off' : 'retain-on-failure',
     screenshot: 'only-on-failure',
     video: process.env.CI ? 'off' : 'retain-on-failure',
 
