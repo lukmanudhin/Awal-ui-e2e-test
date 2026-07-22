00c58d4502b05522cf02a0f5cba102ea1dba5626
Mohammed Lukmanudhin <lukmancolan@gmail.com>
2026-07-22 15:18:28 +0530

    fix: streamline console logs in Sales Enquiry API tests for clarity

--- tests/API/salesEnquiryAPITest.spec.ts
+++ tests/API/salesEnquiryAPITest.spec.ts
@@ -19,87 +19,75 @@ test.describe.serial('Verify Sales Enquiry API', () => {
     });
 
     test('Verify Create Sales Enquiry API', async ({ salesEnquiryAPI }) => {
+        console.log('----------------------Create Sales Enquiry API Response----------------------');
         const createSalesEnquiryAPIResponse = await salesEnquiryAPI.createSalesEnquiryAPI(accessToken, salesEnquiryPayload);
         expect(createSalesEnquiryAPIResponse.message, 'Create Sales Enquiry API Message Mismatch').toBe('Data created successfully');
         enquiryId = createSalesEnquiryAPIResponse.result.extId;
-        console.log('----------------------Create Sales Enquiry API Response----------------------');
-        console.log('API Response:', createSalesEnquiryAPIResponse);
     });
 
     test('Verify Create Embroiding And Tailoring API', async ({ salesEnquiryAPI }) => {
+        console.log('----------------------Create Embroidery API Response-------------------------');
         const createEmbroideryAPIResponse = await salesEnquiryAPI.createEmbroideryAPI(accessToken, enquiryId);
         expect(createEmbroideryAPIResponse.message, 'Embroidery API Message Mismatch').toBe('Data created successfully');
-        console.log('----------------------Create Embroidery API Response-------------------------');
-        console.log('API Response:', createEmbroideryAPIResponse);
     });
 
     test('Verify Create Acrylic Products API', async ({ salesEnquiryAPI }) => {
+        console.log('----------------------Create Acrylic Products API Response------------------');
         const acrylicProductsAPIResponse = await salesEnquiryAPI.createAcrylicProducts(accessToken, enquiryId);
         expect(acrylicProductsAPIResponse.message, 'Acrylic Products API Message Mismatch').toBe('Data created successfully');
-        console.log('----------------------Create Acrylic Products API Response------------------');
-        console.log('API Response:', acrylicProductsAPIResponse);
     });
-    
+
     test('Verify Create Signage Illuminated API', async ({ salesEnquiryAPI }) => {
+        console.log('----------------------Create Signage Illuminated API Response------------------');
         const signageIlluminatedAPIResponse = await salesEnquiryAPI.createSignageIlluminatedAPI(accessToken, enquiryId);
         expect(signageIlluminatedAPIResponse.message, 'Signage Illuminated API Message Mismatch').toBe('Data created successfully');
-        console.log('----------------------Create Signage Illuminated API Response------------------');
-        console.log('API Response:', signageIlluminatedAPIResponse);
     });
 
     test('Verify Create ATM Products API', async ({ salesEnquiryAPI }) => {
+        console.log('----------------------Create ATM Products API Response------------------');
         const atmProductsAPIResponse = await salesEnquiryAPI.createAtmProductsAPI(accessToken, enquiryId);
         expect(atmProductsAPIResponse.message, 'ATM Products API Message Mismatch').toBe('Data created successfully');
-        console.log('----------------------Create ATM Products API Response------------------');
-        console.log('API Response:', atmProductsAPIResponse);
     });
 
     test('Verify Create Vinyl Graphic API', async ({ salesEnquiryAPI }) => {
+        console.log('----------------------Create Vinyl Graphic API Response------------------');
         const vinylGraphicAPIResponse = await salesEnquiryAPI.createVinylGraphicAPI(accessToken, enquiryId);
         expect(vinylGraphicAPIResponse.message, 'Vinyl Graphic API Message Mismatch').toBe('Data created successfully');
-        console.log('----------------------Create Vinyl Graphic API Response------------------');
-        console.log('API Response:', vinylGraphicAPIResponse);
     });
 
     test('Verify Create Trading Products API', async ({ salesEnquiryAPI }) => {
+        console.log('----------------------Create Trading Products API Response------------------');
         const tradingProductsAPIResponse = await salesEnquiryAPI.createTradingProductsAPI(accessToken, enquiryId);
         expect(tradingProductsAPIResponse.message, 'Trading Products API Message Mismatch').toBe('Data created successfully');
-        console.log('----------------------Create Trading Products API Response------------------');
-        console.log('API Response:', tradingProductsAPIResponse);
     });
 
     test('Verify Create PVC Products API', async ({ salesEnquiryAPI }) => {
+        console.log('----------------------Create PVC Products API Response------------------');
         const pvcProductsAPIResponse = await salesEnquiryAPI.createPvcProductsAPI(accessToken, enquiryId);
         expect(pvcProductsAPIResponse.message, 'PVC Products API Message Mismatch').toBe('Data created successfully');
-        console.log('----------------------Create PVC Products API Response------------------');
-        console.log('API Response:', pvcProductsAPIResponse);
     });
 
     test('Verify View Sales Enquiry API', async ({ salesEnquiryAPI }) => {
+        console.log('----------------------View Sales Enquiry API Response------------------------');
         const viewSalesEnquiryAPIResponse = await salesEnquiryAPI.viewSalesEnquiryAPI(accessToken, enquiryId);
         expect(viewSalesEnquiryAPIResponse.message, 'View Sales Enquiry API Message Mismatch').toBe('Data fetched successfully');
-        console.log('----------------------View Sales Enquiry API Response------------------------');
-        console.log('API Response:', viewSalesEnquiryAPIResponse);
     });
 
     test('Verify Edit Sales Enquiry API', async ({ salesEnquiryAPI }) => {
+        console.log('----------------------Edit Sales Enquiry API Response-------------------------');
         const payload = updateEnquiryPayload(enquiryId);
         const editSalesEnquiryAPIResponse = await salesEnquiryAPI.editSalesEnquiryAPI(accessToken, payload);
         expect(editSalesEnquiryAPIResponse.message, 'Edit Sales Enquiry API Message Mismatch').toBe('Data updated successfully');
-        console.log('----------------------Edit Sales Enquiry API Response-------------------------');
-        console.log('API Response:', editSalesEnquiryAPIResponse);
+        console.log('----------------------View Updated Sales Enquiry API Response------------------------');
         const viewSalesEnquiryAPIResponse = await salesEnquiryAPI.viewSalesEnquiryAPI(accessToken, enquiryId);
         expect(viewSalesEnquiryAPIResponse.message, 'View Sales Enquiry API Message Mismatch').toBe('Data fetched successfully');
-        console.log('----------------------View Updated Sales Enquiry API Response------------------------');
-        console.log('API Response:', viewSalesEnquiryAPIResponse);
         await salesEnquiryAPI.validateViewSalesEnquiryAPIResponse(viewSalesEnquiryAPIResponse, payload);
     });
 
     test('Verify Delete Sales Enquiry API', async ({ salesEnquiryAPI }) => {
+        console.log('----------------------Delete Sales Enquiry API Response---------------------');
         const deleteAPIResponse = await salesEnquiryAPI.deleteSalesEnquiry(accessToken, enquiryId);
         expect(deleteAPIResponse.message, 'Delete Sales Enquiry API Message Mismatch').toBe('Data deleted successfully');
-        console.log('----------------------Delete Sales Enquiry API Response---------------------');
-        console.log('API Response:', deleteAPIResponse);
     });
 });
 
