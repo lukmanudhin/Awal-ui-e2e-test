e3b63f2b47b6639bf0e5340169478d0030b28a2a
Mohammed Lukmanudhin <lukmancolan@gmail.com>
2026-09-02 14:41:42 +0530

    fix: Update edit icon label from 'Delivery Days' to 'Delivery Period'

--- pages/procurementPage.ts
+++ pages/procurementPage.ts
@@ -274,7 +274,7 @@ export class ProcurementPage extends BasePage {
         await this.saveMaterialRowBtn.click();
 
         await this.page.mouse.wheel(0, 1000);
-        await this.editIcons('Delivery Days').click();
+        await this.editIcons('Delivery Period').click();
         await this.page.locator('//input[@maxlength="100"]').fill(mirDetails.deliveryPeriod);
         await this.tickIcon.click();
 
