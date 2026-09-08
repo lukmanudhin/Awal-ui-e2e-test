a8fd677372ec18b0ef48dea10c6370c865f27604
Mohammed Lukmanudhin <lukmancolan@gmail.com>
2026-09-08 17:09:30 +0530

    fix: Update comment for clarity on delivery period editing

--- pages/procurementPage.ts
+++ pages/procurementPage.ts
@@ -270,7 +270,7 @@ export class ProcurementPage extends BasePage {
         await this.saveMaterialRowBtn.click();
 
         await this.page.mouse.wheel(0, 1000);
-        // inconsistent need to ask
+        // inconsistent need to update
         await this.editIcons('Delivery Days').or(this.editIcons('Delivery Period')).click();
         await this.page.locator('//input[@maxlength="100"]').fill(mirDetails.deliveryPeriod);
         await this.tickIcon.click();
