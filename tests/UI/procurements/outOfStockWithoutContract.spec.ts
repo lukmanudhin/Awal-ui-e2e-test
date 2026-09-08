05b802a2fd24eb6cf17bd424e0ded422099c2086
Mohammed Lukmanudhin <lukmancolan@gmail.com>
2026-09-08 17:10:39 +0530

    fix: Update vendor quote approval status to 207 for validation

--- tests/UI/procurements/outOfStockWithoutContract.spec.ts
+++ tests/UI/procurements/outOfStockWithoutContract.spec.ts
@@ -197,7 +197,8 @@ test.describe('Material Indent and Material Issue For Out of Stock Raw Material 
             await modules.goToModule({ nestedSubModule: 'Vendor Quote Comparison (Manager)' });
             await procurementPage.search(prId);
             await procurementPage.clickViewIcon();
-            await procurementPage.approveVendorQuoteAndValidateAPI(200);
+            //need to ask 207 Multi status
+            await procurementPage.approveVendorQuoteAndValidateAPI(207);
             await expect(procurementPage.successMessage('Data updated successfully'), 'Data updated successfully message does not match').toHaveText('Data updated successfully');
         });
 
