84087859a6e148c9e9a92838eb3fd49fe227bae5
Mohammed Lukmanudhin <lukmancolan@gmail.com>
2026-09-15 19:35:10 +0530

    fix: Update locator for version and option validation in QuotationManagerPage

--- pages/quotationManagerPage.ts
+++ pages/quotationManagerPage.ts
@@ -337,7 +337,7 @@ export class QuotationManagerPage extends BasePage {
     }
     @step()
     async validateVersion2AndOption1Visible(version: string, option: string) {
-        const innerText = await this.page.locator('//div[@class="border-l-2 border-gray-200 pl-8 text-black-10"]').innerText();
+        const innerText = await this.page.locator('//div[@class="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-0 mb-3 text-black-10"]').innerText();
         expect(innerText, `Version and option details do not contain version: ${version}`).toContain(version);
         expect(innerText, `Version and option details do not contain option: ${option}`).toContain(option);
     }
